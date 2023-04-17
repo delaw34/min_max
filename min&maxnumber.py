539993290f652ce6a1dc90c5f92b1f8bc98e66f0
delaw34 <delaw34@yahoo.com>
2023-04-17 23:59:10 +0100

    first commit

---
+++ min&maxnumber.py
@@ -0,0 +1,8 @@
+# Python program with 15 items in a list
+# Print the largest and smallest number from the list
+
+numbers = [20,11,15,30,40,70,60,100,112]
+max_number = max(numbers)
+min_number = min(numbers)
+
+print("this are the highest",max_number,"and the lowest number", min_number,"on the list")
